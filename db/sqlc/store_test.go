141fb78e63d2834ae4d1d84d6dbe621774ff3313
Sayan Mallick <snyxmk@gmail.com>
2023-04-10 15:56:48 +0530

    failed store test

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,75 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTranferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	n := 5
+	amount := int64(10)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	// run n concurrent transfer transaction
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTx(context.Background(), CreateTransferParams{
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
+				Amount:        amount,
+			})
+
+			errs <- err
+			results <- result
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		transfer := result.Transfer
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, account1.ID, fromEntry.AccountID)
+		require.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		ToEntry := result.FromEntry
+		require.NotEmpty(t, ToEntry)
+		require.Equal(t, account1.ID, ToEntry.AccountID)
+		require.Equal(t, -amount, ToEntry.Amount)
+		require.NotZero(t, ToEntry.ID)
+		require.NotZero(t, ToEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), ToEntry.ID)
+		require.NoError(t, err)
+
+	}
+
+}
